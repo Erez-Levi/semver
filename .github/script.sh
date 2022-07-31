08964e3eac3ecc047ee1aa1529ca56f08da0b32d
Erez Levy <59343752+Erez-Levi@users.noreply.github.com>
2022-07-31 19:11:37 +0300

    Update script.sh

--- .github/script.sh
+++ .github/script.sh
@@ -1,7 +1,7 @@
 string=${{ github.event.head_commit.message }}
 if [[ $string == *"feat"* ]]; then
-  .github/increment_version.sh -m $curr_tag
+  newtag=$(.github/increment_version.sh -m $curr_tag)
 elif [[ $string == *"BREAKING CHANGE"* ]]; then
-  .github/increment_version.sh -M $curr_tag
+  newtag-$(.github/increment_version.sh -M $curr_tag)
 else
-  .github/increment_version.sh -p $curr_ta
+  newtag=$(.github/increment_version.sh -p $curr_ta)
